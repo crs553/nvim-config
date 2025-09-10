0297964a3c94ecdf01109d3acb13fdff9995ca4f
charlie-lto <charlie@leedstestobjects.com>
2025-09-10 16:38:03 +0100

    fix: snacks

---
+++ ftplugin/matlab.lua
@@ -0,0 +1,12 @@
+-- ~/.config/nvim/ftplugin/matlab.lua
+
+-- Comment string
+vim.bo.commentstring = "% %s"
+
+-- Indentation
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
+
+-- Folding
+vim.wo.foldmethod = "indent"
