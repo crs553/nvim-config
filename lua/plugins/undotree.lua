cca46333773857ade1ccabc159477e8031447347
charlie-lto <charlie@leedstestobjects.com>
2026-04-01 12:18:25 +0100

    fix: overlapping keymap

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
-  {
-    src = "https://github.com/mbbill/undotree",
-  },
+	{
+		src = "https://github.com/mbbill/undotree",
+	},
 })
 
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
+vim.keymap.set("n", "<leader>uu", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
