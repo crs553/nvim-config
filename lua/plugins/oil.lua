825011b86da7ebccdbe3cf0babbde502ed2db56a
charlie-lto <charlie@leedstestobjects.com>
2026-04-01 10:29:54 +0100

    fix: show hidden files in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,12 +1,12 @@
 vim.pack.add({
-  {
-    src = "https://github.com/stevearc/oil.nvim",
-  }
+	{
+		src = "https://github.com/stevearc/oil.nvim",
+	},
 })
 require("oil").setup({
-  default_file_explorer = true,
-  view_options = {
-    show_hidden = false,
-  },
+	default_file_explorer = true,
+	view_options = {
+		show_hidden = true,
+	},
 })
 vim.keymap.set("n", "-", "<cmd>Oil<CR>")
