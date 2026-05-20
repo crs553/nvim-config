f7a8f4e6a0e912ab3978b57627dc34eda7c52920
crs553 <42813301+crs553@users.noreply.github.com>
2026-05-20 10:40:47 +0100

    feat(whickkey): more detailed keymaps

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -3,8 +3,26 @@ vim.pack.add({
 	"https://github.com/nvim-mini/mini.icons",
 })
 
-require("which-key").setup({})
+local wk = require("which-key")
+wk.setup({})
 
 vim.keymap.set("n", "<leader>?", function()
-	require("which-key").show({ global = false })
+	wk.show({ global = false })
 end, { desc = "Buffer Local Keymaps (which-key)" })
+
+-- add groups
+wk.add({
+	{ "<leader>a", group = "AI" },
+	{ "<leader>ac", group = "CLI" },
+	{ "<leader>b", group = "Buffers" },
+	{ "<leader>d", group = "DAP" },
+	{ "<leader>f", group = "Find..." },
+	{ "<leader>fi", group = "Find In ..." },
+	{ "<leader>g", group = "Git" },
+	{ "<leader>l", group = "LSP" },
+	{ "<leader>q", group = "Quickfix" },
+	{ "<leader>s", group = "Search In ..." },
+	{ "<leader>u", group = "Undo ..." },
+	{ "<leader>v", group = "Vim Pack ..." },
+	{ "<leader><leader>", group = "Source..." },
+})
