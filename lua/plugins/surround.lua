66fab4a79fe20d48c671db617060353555713b44
crs553 <42813301+crs553@users.noreply.github.com>
2026-05-21 11:05:15 +0100

    fix(surround): nvim surround setup

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,5 +1,7 @@
 vim.pack.add({
-  {
-    src = "https://github.com/kylechui/nvim-surround",
-  },
+	{
+		src = "https://github.com/kylechui/nvim-surround",
+	},
 })
+
+require("nvim-surround").setup()
