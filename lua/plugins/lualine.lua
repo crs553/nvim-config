f460ad2d8dfe50fc03f1675d2f09b7a630202858
charlie-lto <charlie@leedstestobjects.com>
2026-05-27 10:37:52 +0100

    feat(lualine): improvements

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,40 @@
 -- Lualine setup
-require("lualine").setup({
-	options = {
-		theme = "auto",
-		section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
-		globalstatus = true, -- requires Neovim 0.8+
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-})
+require('lualine').setup {
+  options = {
+    theme = 'auto',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    globalstatus = true,
+    disabled_filetypes = {
+      'NvimTree',
+      'neo-tree',
+      'TelescopePrompt',
+      'alpha',
+      'dashboard',
+    },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = {
+      { 'filename', file_status = true, path = 1 },
+    },
+    lualine_x = {
+      'encoding',
+      'fileformat',
+      'filetype',
+      { 'progress', separator = ' ', padding = 1 },
+      { 'diagnostics', sources = { 'nvim_diagnostic', 'nvim_lsp' } },
+    },
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {
+    lualine_a = { 'buffers' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'tabs' },
+  },
+}
