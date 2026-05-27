27be242024f1e038b65d445ee61f9b03fc297221
crs553 <42813301+crs553@users.noreply.github.com>
2026-05-27 11:00:12 +0100

    feat(colorschemes): highlighting

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,31 +1,53 @@
-require("catppuccin").setup({
-	flavour = "mocha",
-	transparent_background = false,
-	styles = {
-		comments = { "italic" },
-		conditionals = { "italic" },
-	},
-	integrations = {
-		treesitter = true,
-		native_lsp = { enabled = true },
-		telescope = true,
-		which_key = true,
-		cmp = true,
-		lualine = true,
-		gitsigns = true,
-		dap = { enabled = true },
-		oil = true,
-		markdown = true,
-		mini = { enabled = true },
-		snacks = { enabled = true },
-	},
+require('catppuccin').setup {
+  flavour = 'mocha',
+  transparent_background = false,
+  styles = {
+    comments = { 'italic' },
+    conditionals = { 'italic' },
+  },
+  integrations = {
+    treesitter = true,
+    native_lsp = { enabled = true },
+    telescope = true,
+    which_key = true,
+    cmp = true,
+    lualine = true,
+    gitsigns = true,
+    dap = { enabled = true },
+    oil = true,
+    markdown = true,
+    mini = { enabled = true },
+    snacks = { enabled = true },
+  },
 
-	custom_highlights = function(colors)
-		return {
-			LineNr = { fg = colors.overlay1 },
-			CursorLineNr = { fg = colors.pink, style = { "bold" } },
-			CursorLine = { bg = colors.surface0 },
-		}
-	end,
-})
+  custom_highlights = function(colors)
+    return {
+      -- Line numbers
+      LineNr = { fg = colors.overlay1 },
+      CursorLineNr = { fg = colors.mauve, style = { 'bold' } },
+      CursorLine = { bg = colors.surface0 },
+
+      -- Visual selection
+      Visual = { bg = colors.surface1 },
+
+      -- Search highlights
+      Search = { bg = colors.mauve, fg = colors.base },
+      IncSearch = { bg = colors.pink, fg = colors.base },
+
+      -- Telescope
+      TelescopeBorder = { fg = colors.mauve },
+      TelescopePromptBorder = { fg = colors.mauve },
+      TelescopeSelection = { bg = colors.surface0 },
+
+      -- CMP completion menu
+      PmenuSel = { bg = colors.mauve, fg = colors.base },
+
+      -- WhichKey
+      WhichKey = { fg = colors.mauve },
+
+      -- Floating windows
+      FloatBorder = { fg = colors.mauve },
+    }
+  end,
+}
 vim.cmd([[colorscheme catppuccin-mocha]])
