90d6e49d26c01ec8c944f420d05e5434650c522b
crs553 <42813301+crs553@users.noreply.github.com>
2026-05-29 20:39:49 +0100

    fix: removal of experimental neovim ui

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,32 +25,32 @@ opt.hlsearch = true -- highlight search matches
 opt.incsearch = true -- show matches as you type
 
 -- Preview substitutions
-opt.signcolumn = "yes" -- akways show a signcolumn
+opt.signcolumn = 'yes' -- akways show a signcolumn
 opt.showmatch = true -- highlight matching brackets
-opt.completeopt = "menuone,noinsert,noselect" -- completion o[tions
+opt.completeopt = 'menuone,noinsert,noselect' -- completion o[tions
 opt.showmode = false
 opt.pumheight = 10
 opt.pumblend = 10
 opt.winblend = 0
 opt.conceallevel = 0
-opt.concealcursor = ""
+opt.concealcursor = ''
 opt.synmaxcol = 300
-opt.inccommand = "split"
-opt.fillchars = { eob = " " }
+opt.inccommand = 'split'
+opt.fillchars = { eob = ' ' }
 
 -- save undo history
 opt.undofile = true
 opt.autoread = true -- auto-reload changes if outside of neovim
 opt.autowrite = false -- do not auto-save
 
-opt.backspace = "indent,eol,start" -- better backspace behaviour
-opt.path:append("*") --include subdirs in search
-opt.selection = "inclusive" -- include last char in selection
-opt.mouse = "a" -- enable mouse support
+opt.backspace = 'indent,eol,start' -- better backspace behaviour
+opt.path:append('*') --include subdirs in search
+opt.selection = 'inclusive' -- include last char in selection
+opt.mouse = 'a' -- enable mouse support
 
 -- Folding: requires treesitter available at runtime; safe fallback if not
-opt.foldmethod = "expr" -- use expression for folding
-opt.foldexpr = "nvim_treesitter#foldexpr()" -- use treesitter for folding
+opt.foldmethod = 'expr' -- use expression for folding
+opt.foldexpr = 'nvim_treesitter#foldexpr()' -- use treesitter for folding
 opt.foldlevel = 99 -- start with all folds open
 
 opt.splitbelow = true -- horizontal split below
@@ -60,24 +60,23 @@ opt.title = true -- Set window title
 opt.showcmd = true -- Show (partial) command in the last line
 
 -- diagnostic tools for lsp
-vim.diagnostic.config({
-	virtual_text = true,
-	virtual_line = true,
-	signs = true,
-	update_in_insert = false,
-	underline = true,
-	severity_sort = true,
-	float = {
-		border = "rounded",
-		source = "if_many",
-		header = "",
-		prefix = "",
-	},
-})
+vim.diagnostic.config {
+  virtual_text = true,
+  virtual_line = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'if_many',
+    header = '',
+    prefix = '',
+  },
+}
 
 -- Inlay hints (enabled by default, toggle with <leader>uh)
 vim.lsp.inlay_hint.enable(true, nil)
 
-
 -- Experimental ui for neovim
-require("vim._core.ui2").enable({})
+--require("vim._core.ui2").enable({})
