cd717f52408ebc8033c37b99d4698de95a6e9b13
crs553 <42813301+crs553@users.noreply.github.com>
2026-08-05 10:31:01 +0100

    feat(gitsigns): use compact diff sign symbols

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,1 +1,9 @@
-require('gitsigns').setup {}
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' }, ---@diagnostic disable-line: missing-fields
+    change = { text = '~' }, ---@diagnostic disable-line: missing-fields
+    delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
+    topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
+    changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
+  },
+}
