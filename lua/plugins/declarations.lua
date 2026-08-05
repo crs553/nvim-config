cf853c97be9a0ae64ad15d4d1f2fe89fa9a8bedf
crs553 <42813301+crs553@users.noreply.github.com>
2026-08-05 10:30:52 +0100

    feat(plugins): add indent-blankline indent guides with pack build hooks

--- lua/plugins/declarations.lua
+++ lua/plugins/declarations.lua
@@ -1,97 +1,147 @@
+local function run_build(name, cmd, cwd)
+  local result = vim.system(cmd, { cwd = cwd }):wait()
+  if result.code ~= 0 then
+    local stderr = result.stderr or ''
+    local stdout = result.stdout or ''
+    local output = stderr ~= '' and stderr or stdout
+    if output == '' then output = 'No output from build command.' end
+    vim.notify(('Build failed for %s:\n%s'):format(name, output), vim.log.levels.ERROR)
+  end
+end
+
+-- This autocommand runs after a plugin is installed or updated and
+--  runs the appropriate build command for that plugin if necessary.
+--
+-- See `:help vim.pack-events`
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    local name = ev.data.spec.name
+    local kind = ev.data.kind
+    if kind ~= 'install' and kind ~= 'update' then return end
+
+    if name == 'telescope-fzf-native.nvim' and vim.fn.executable 'make' == 1 then
+      run_build(name, { 'make' }, ev.data.path)
+      return
+    end
+
+    if name == 'LuaSnip' then
+      if vim.fn.has 'win32' ~= 1 and vim.fn.executable 'make' == 1 then
+        run_build(name, { 'make', 'install_jsregexp' }, ev.data.path)
+      end
+      return
+    end
+
+    if name == 'nvim-treesitter' then
+      if not ev.data.active then vim.cmd.packadd 'nvim-treesitter' end
+      vim.cmd 'TSUpdate'
+      return
+    end
+  end,
+})
+
+---Because most plugins are hosted on GitHub, you can use the helper
+---function to have less repetition in the following sections.
+---@param repo string
+---@return string
+local function gh(repo) return 'https://github.com/' .. repo end
+
 -- Foundational (declare first so other plugins can require them)
-vim.pack.add { 'https://github.com/nvim-lua/plenary.nvim' }
+vim.pack.add { gh 'nvim-lua/plenary.nvim' }
 
 -- Colorscheme
-vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
+vim.pack.add { { src = gh 'catppuccin/nvim', name = 'catppuccin' } }
 
 -- UI / Visual
 vim.pack.add {
-  'https://github.com/nvim-lualine/lualine.nvim',
-  'https://github.com/nvim-tree/nvim-web-devicons',
+  gh 'nvim-lualine/lualine.nvim',
+  gh 'nvim-tree/nvim-web-devicons',
 }
 vim.pack.add {
-  'https://github.com/folke/which-key.nvim',
-  'https://github.com/nvim-mini/mini.icons',
+  gh 'folke/which-key.nvim',
+  gh 'nvim-mini/mini.icons',
 }
 vim.pack.add {
-  { src = 'https://github.com/folke/snacks.nvim' },
+  { src = gh 'folke/snacks.nvim' },
 }
 
 -- Editor features
-vim.pack.add { 'https://github.com/lewis6991/gitsigns.nvim' }
+vim.pack.add { gh 'lewis6991/gitsigns.nvim' }
 vim.pack.add {
   {
-    src = 'https://github.com/kylechui/nvim-surround',
+    src = gh 'kylechui/nvim-surround',
   },
 }
-vim.pack.add { { src = 'https://github.com/nvim-mini/mini.comment', version = 'stable' } }
+vim.pack.add { { src = gh 'nvim-mini/mini.comment', version = 'stable' } }
 vim.pack.add {
-  { src = 'https://github.com/stevearc/conform.nvim' },
+  { src = gh 'stevearc/conform.nvim' },
 }
 vim.pack.add {
-  'https://github.com/mfussenegger/nvim-lint',
-  'https://github.com/rshkarin/mason-nvim-lint',
+  gh 'mfussenegger/nvim-lint',
+  gh 'rshkarin/mason-nvim-lint',
 }
+
+-- Indentation guides
+vim.pack.add { gh 'lukas-reineke/indent-blankline.nvim' }
 
 -- Treesitter
 vim.pack.add {
   {
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    src = gh 'nvim-treesitter/nvim-treesitter',
     commit = '4916d6592ede8c07973490d9322f187e07dfefac',
   },
 }
 
 -- LSP
-vim.pack.add { { src = 'https://github.com/neovim/nvim-lspconfig' } }
-vim.pack.add { { src = 'https://github.com/williamboman/mason.nvim' } }
-vim.pack.add { { src = 'https://github.com/williamboman/mason-lspconfig.nvim' } }
+vim.pack.add { { src = gh 'neovim/nvim-lspconfig' } }
+vim.pack.add { { src = gh 'williamboman/mason.nvim' } }
+vim.pack.add { { src = gh 'williamboman/mason-lspconfig.nvim' } }
 
 -- CMP & Snippets
 vim.pack.add {
-  { src = 'https://github.com/hrsh7th/nvim-cmp' },
-  { src = 'https://github.com/hrsh7th/cmp-nvim-lsp' },
-  { src = 'https://github.com/hrsh7th/cmp-buffer' },
-  { src = 'https://github.com/hrsh7th/cmp-path' },
-  { src = 'https://github.com/hrsh7th/cmp-cmdline' },
-  { src = 'https://github.com/L3MON4D3/LuaSnip' },
-  { src = 'https://github.com/rafamadriz/friendly-snippets' },
-  { src = 'https://github.com/tzachar/cmp-ai' },
+  { src = gh 'hrsh7th/nvim-cmp' },
+  { src = gh 'hrsh7th/cmp-nvim-lsp' },
+  { src = gh 'hrsh7th/cmp-buffer' },
+  { src = gh 'hrsh7th/cmp-path' },
+  { src = gh 'hrsh7th/cmp-cmdline' },
+  { src = gh 'L3MON4D3/LuaSnip' },
+  { src = gh 'rafamadriz/friendly-snippets' },
+  { src = gh 'tzachar/cmp-ai' },
 }
 
 -- AI
 vim.pack.add {
-  { src = 'https://github.com/olimorris/codecompanion.nvim', version = 'v19.13.0' },
-  { src = 'https://github.com/nvim-mini/mini.diff' },
+  { src = gh 'olimorris/codecompanion.nvim', version = 'v19.13.0' },
+  { src = gh 'nvim-mini/mini.diff' },
 }
 
 -- DAP
 vim.pack.add {
   { src = 'https://codeberg.org/mfussenegger/nvim-dap.git' },
-  { src = 'https://github.com/rcarriga/nvim-dap-ui' },
-  { src = 'https://github.com/theHamsta/nvim-dap-virtual-text' },
-  { src = 'https://github.com/leoluz/nvim-dap-go' },
-  { src = 'https://github.com/mfussenegger/nvim-dap-python' },
-  { src = 'https://github.com/nvim-neotest/nvim-nio' },
+  { src = gh 'rcarriga/nvim-dap-ui' },
+  { src = gh 'theHamsta/nvim-dap-virtual-text' },
+  { src = gh 'leoluz/nvim-dap-go' },
+  { src = gh 'mfussenegger/nvim-dap-python' },
+  { src = gh 'nvim-neotest/nvim-nio' },
 }
 
 -- Telescope
 vim.pack.add {
-  'https://github.com/nvim-telescope/telescope.nvim',
-  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  gh 'nvim-telescope/telescope.nvim',
+  gh 'nvim-telescope/telescope-fzf-native.nvim',
 }
 
 -- File navigation
-vim.pack.add { { src = 'https://github.com/stevearc/oil.nvim' } }
+vim.pack.add { { src = gh 'stevearc/oil.nvim' } }
 vim.pack.add {
-  'https://github.com/tadmccorkle/markdown.nvim',
-  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  gh 'tadmccorkle/markdown.nvim',
+  gh 'MeanderingProgrammer/render-markdown.nvim',
 }
 vim.pack.add {
-  { src = 'https://github.com/obsidian-nvim/obsidian.nvim', version = 'v3.16.3' },
+  { src = gh 'obsidian-nvim/obsidian.nvim', version = 'v3.16.3' },
 }
-vim.pack.add { { src = 'https://github.com/stevearc/quicker.nvim.git' } }
+vim.pack.add { { src = gh 'stevearc/quicker.nvim' } }
 
 -- Other
 vim.pack.add {
-  'https://github.com/theprimeagen/vim-be-good',
+  gh 'theprimeagen/vim-be-good',
 }
